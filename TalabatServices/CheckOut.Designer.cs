6842d7344c9d5463cb49fad935b7feb43448b605
Ahmed3280 <ahmedakram4292@gmail.com>
2024-12-21 00:05:49 +0200

    Check Out Design and Code Done.

--- TalabatServices/CheckOut.Designer.cs
+++ TalabatServices/CheckOut.Designer.cs
@@ -29,19 +29,83 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            dataGridView1 = new DataGridView();
+            label2 = new Label();
+            button2 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
             button1.BackColor = Color.FromArgb(192, 0, 0);
+            button1.FlatStyle = FlatStyle.Popup;
             button1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.Location = new Point(684, 12);
+            button1.Location = new Point(12, 497);
             button1.Name = "button1";
-            button1.Size = new Size(104, 60);
+            button1.Size = new Size(120, 67);
             button1.TabIndex = 9;
             button1.Text = "Exit";
             button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(43, 96);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(382, 294);
+            dataGridView1.TabIndex = 10;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Stencil", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.FromArgb(41, 128, 185);
+            label2.Location = new Point(122, 24);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(201, 40);
+            label2.TabIndex = 11;
+            label2.Text = "Checkout";
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Cyan;
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(349, 497);
+            button2.Name = "button2";
+            button2.Size = new Size(120, 67);
+            button2.TabIndex = 12;
+            button2.Text = "Done";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Stencil", 16.2F, FontStyle.Bold);
+            label1.ForeColor = Color.FromArgb(41, 128, 185);
+            label1.Location = new Point(13, 432);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(104, 33);
+            label1.TabIndex = 13;
+            label1.Text = "Total";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ButtonFace;
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Location = new Point(125, 432);
+            textBox1.Margin = new Padding(4);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(277, 27);
+            textBox1.TabIndex = 14;
             // 
             // CheckOut
             // 
@@ -50,19 +114,32 @@
             BackColor = Color.White;
             BackgroundImage = Properties.Resources.Proj_pic;
             BackgroundImageLayout = ImageLayout.Zoom;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(481, 576);
             ControlBox = false;
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(label2);
+            Controls.Add(dataGridView1);
             Controls.Add(button1);
             FormBorderStyle = FormBorderStyle.None;
             MaximizeBox = false;
             Name = "CheckOut";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "CheckOut";
+            Load += CheckOut_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
+        private DataGridView dataGridView1;
+        private Label label2;
+        private Button button2;
+        private Label label1;
+        private TextBox textBox1;
     }
 }
--- TalabatServices/CheckOut.cs
+++ TalabatServices/CheckOut.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,33 @@ namespace TalabatServices
         private void button1_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void CheckOut_Load(object sender, EventArgs e)
+        {
+            //LoadData();
+        }
+        
+        //private void LoadData()
+        //{
+        //    string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DbConnection"].ConnectionString;
+
+        //    using (SqlConnection connection = new SqlConnection(connectionString))
+        //    {
+        //        string query = "SELECT Name, Quantity, Price FROM Items";
+        //        SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+        //        DataTable dataTable = new DataTable();
+        //        adapter.Fill(dataTable);
+
+        //        // Bind to DataGridView
+        //        dataGridView1.DataSource = dataTable;
+        //    }
+        //}
+        
+        
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
