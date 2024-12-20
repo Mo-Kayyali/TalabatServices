62a51db92b3af270dac989a93695517f782e5276
Ahmed3280 <ahmedakram4292@gmail.com>
2024-12-20 14:36:20 +0200

    WorkerAcceptedReq

--- TalabatServices/WorkerAcceptedRequest.Designer.cs
+++ TalabatServices/WorkerAcceptedRequest.Designer.cs
@@ -28,18 +28,221 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label2 = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            textBox5 = new TextBox();
+            label6 = new Label();
+            textBox6 = new TextBox();
+            label7 = new Label();
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Stencil", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.FromArgb(41, 128, 185);
+            label2.Location = new Point(76, 30);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(355, 40);
+            label2.TabIndex = 3;
+            label2.Text = "User Information";
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.ButtonFace;
+            textBox3.Location = new Point(199, 227);
+            textBox3.Margin = new Padding(4);
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.Size = new Size(277, 27);
+            textBox3.TabIndex = 12;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Stencil", 16.2F, FontStyle.Bold);
+            label4.ForeColor = Color.FromArgb(41, 128, 185);
+            label4.Location = new Point(17, 227);
+            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(122, 33);
+            label4.TabIndex = 11;
+            label4.Text = "Rating";
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.ButtonFace;
+            textBox2.Location = new Point(199, 169);
+            textBox2.Margin = new Padding(4);
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(277, 27);
+            textBox2.TabIndex = 10;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Stencil", 16.2F, FontStyle.Bold);
+            label3.ForeColor = Color.FromArgb(41, 128, 185);
+            label3.Location = new Point(17, 169);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(112, 33);
+            label3.TabIndex = 9;
+            label3.Text = "Phone";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ButtonFace;
+            textBox1.Location = new Point(199, 109);
+            textBox1.Margin = new Padding(4);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(277, 27);
+            textBox1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Stencil", 16.2F, FontStyle.Bold);
+            label1.ForeColor = Color.FromArgb(41, 128, 185);
+            label1.Location = new Point(17, 109);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(93, 33);
+            label1.TabIndex = 7;
+            label1.Text = "Name";
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = SystemColors.ButtonFace;
+            textBox4.Location = new Point(199, 350);
+            textBox4.Margin = new Padding(4);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.ReadOnly = true;
+            textBox4.Size = new Size(277, 71);
+            textBox4.TabIndex = 14;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Stencil", 16.2F, FontStyle.Bold);
+            label5.ForeColor = Color.FromArgb(41, 128, 185);
+            label5.Location = new Point(17, 363);
+            label5.Margin = new Padding(4, 0, 4, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(143, 33);
+            label5.TabIndex = 13;
+            label5.Text = "Address";
+            // 
+            // textBox5
+            // 
+            textBox5.BackColor = SystemColors.ButtonFace;
+            textBox5.Location = new Point(199, 284);
+            textBox5.Margin = new Padding(4);
+            textBox5.Name = "textBox5";
+            textBox5.ReadOnly = true;
+            textBox5.Size = new Size(277, 27);
+            textBox5.TabIndex = 16;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Stencil", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.FromArgb(41, 128, 185);
+            label6.Location = new Point(17, 278);
+            label6.Margin = new Padding(4, 0, 4, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(134, 33);
+            label6.TabIndex = 15;
+            label6.Text = "Service";
+            // 
+            // textBox6
+            // 
+            textBox6.BackColor = SystemColors.ButtonFace;
+            textBox6.Location = new Point(199, 463);
+            textBox6.Margin = new Padding(4);
+            textBox6.Multiline = true;
+            textBox6.Name = "textBox6";
+            textBox6.ReadOnly = true;
+            textBox6.Size = new Size(277, 106);
+            textBox6.TabIndex = 18;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Stencil", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.FromArgb(41, 128, 185);
+            label7.Location = new Point(17, 463);
+            label7.Margin = new Padding(4, 0, 4, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(170, 27);
+            label7.TabIndex = 17;
+            label7.Text = "Description";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ButtonFace;
+            button1.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(347, 595);
+            button1.Name = "button1";
+            button1.Size = new Size(179, 60);
+            button1.TabIndex = 19;
+            button1.Text = "Arrived";
+            button1.UseVisualStyleBackColor = false;
             // 
             // WorkerAcceptedRequest
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.White;
+            ClientSize = new Size(538, 667);
+            Controls.Add(button1);
+            Controls.Add(textBox6);
+            Controls.Add(label7);
+            Controls.Add(textBox5);
+            Controls.Add(label6);
+            Controls.Add(textBox4);
+            Controls.Add(label5);
+            Controls.Add(textBox3);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(label2);
             Name = "WorkerAcceptedRequest";
             Text = "WorkerAcceptedRequest";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label2;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label1;
+        private TextBox textBox4;
+        private Label label5;
+        private TextBox textBox5;
+        private Label label6;
+        private TextBox textBox6;
+        private Label label7;
+        private Button button1;
     }
 }
