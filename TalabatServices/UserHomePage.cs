02015581c87c8222bd10eb4f6d09d01a39da983e
orkeedwalid <orkeedmandour@gmail.com>
2024-12-21 18:36:01 +0200

    homepage design

--- TalabatServices/UserHomePage.cs
+++ TalabatServices/UserHomePage.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace TalabatServices
 {
@@ -15,6 +16,31 @@ namespace TalabatServices
         public UserHomePage()
         {
             InitializeComponent();
+        }
+
+        private void UserHomePage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn=new SqlConnection()
+            this.Hide();
+            UserMakingRequest UMR = new UserMakingRequest();
+            UMR.Show();
+        }
+
+        private void setting_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            ProfileSettings PS = new ProfileSettings();
+            PS.Show();
         }
     }
 }
--- TalabatServices/UserMakingRequest.cs
+++ TalabatServices/UserMakingRequest.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
+using System.Collections;
 namespace TalabatServices
 {
     public partial class UserMakingRequest : Form
@@ -15,6 +16,33 @@ namespace TalabatServices
         public UserMakingRequest()
         {
             InitializeComponent();
+        }
+       // public string constring = "Data Source=DESKTOP;Initial Catalog=TalabatServices;Integrated Security=True;Trust Server Certificate=True";
+
+        //string query = "SELECT S_ID, NAME FROM Services";
+
+
+
+
+
+        private void UserMakingRequest_Load(object sender, EventArgs e)
+        {
+          //  SqlConnection con = new SqlConnection(constring);
+           // con.Open();
+           // SqlCommand command =new SqlCommand(query, con);
+
+        }
+
+        private void chooseservice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void back_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            UserHomePage UHP = new UserHomePage();
+            UHP.Show();
         }
     }
 }
