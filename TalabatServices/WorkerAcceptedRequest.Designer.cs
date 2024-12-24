b65e2536260697bc33e5f67be250cba85698ea26
Kayyali <mohamedelkayyali2@gmail.com>
2024-12-24 02:27:57 +0200

    Done workeraccpetedrequest textbox

--- TalabatServices/WorkerAcceptedRequest.Designer.cs
+++ TalabatServices/WorkerAcceptedRequest.Designer.cs
@@ -47,10 +47,10 @@
             label2.AutoSize = true;
             label2.Font = new Font("Stencil", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label2.ForeColor = Color.FromArgb(41, 128, 185);
-            label2.Location = new Point(95, 38);
-            label2.Margin = new Padding(5, 0, 5, 0);
+            label2.Location = new Point(76, 30);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(413, 47);
+            label2.Size = new Size(355, 40);
             label2.TabIndex = 3;
             label2.Text = "User Information";
             // 
@@ -58,11 +58,12 @@
             // 
             Phone_tb.BackColor = SystemColors.ButtonFace;
             Phone_tb.BorderStyle = BorderStyle.FixedSingle;
-            Phone_tb.Location = new Point(249, 211);
-            Phone_tb.Margin = new Padding(5, 5, 5, 5);
+            Phone_tb.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            Phone_tb.Location = new Point(199, 169);
+            Phone_tb.Margin = new Padding(4, 4, 4, 4);
             Phone_tb.Name = "Phone_tb";
             Phone_tb.ReadOnly = true;
-            Phone_tb.Size = new Size(346, 31);
+            Phone_tb.Size = new Size(277, 34);
             Phone_tb.TabIndex = 10;
             // 
             // label3
@@ -70,10 +71,10 @@
             label3.AutoSize = true;
             label3.Font = new Font("Stencil", 16.2F, FontStyle.Bold);
             label3.ForeColor = Color.FromArgb(41, 128, 185);
-            label3.Location = new Point(21, 211);
-            label3.Margin = new Padding(5, 0, 5, 0);
+            label3.Location = new Point(17, 169);
+            label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
-            label3.Size = new Size(130, 39);
+            label3.Size = new Size(112, 33);
             label3.TabIndex = 9;
             label3.Text = "Phone";
             // 
@@ -81,11 +82,12 @@
             // 
             Name_tb.BackColor = SystemColors.ButtonFace;
             Name_tb.BorderStyle = BorderStyle.FixedSingle;
-            Name_tb.Location = new Point(249, 136);
-            Name_tb.Margin = new Padding(5, 5, 5, 5);
+            Name_tb.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            Name_tb.Location = new Point(199, 109);
+            Name_tb.Margin = new Padding(4, 4, 4, 4);
             Name_tb.Name = "Name_tb";
             Name_tb.ReadOnly = true;
-            Name_tb.Size = new Size(346, 31);
+            Name_tb.Size = new Size(277, 34);
             Name_tb.TabIndex = 8;
             // 
             // label1
@@ -93,10 +95,10 @@
             label1.AutoSize = true;
             label1.Font = new Font("Stencil", 16.2F, FontStyle.Bold);
             label1.ForeColor = Color.FromArgb(41, 128, 185);
-            label1.Location = new Point(21, 136);
-            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Location = new Point(17, 109);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(108, 39);
+            label1.Size = new Size(93, 33);
             label1.TabIndex = 7;
             label1.Text = "Name";
             // 
@@ -104,12 +106,13 @@
             // 
             Addr_tb.BackColor = SystemColors.ButtonFace;
             Addr_tb.BorderStyle = BorderStyle.FixedSingle;
-            Addr_tb.Location = new Point(249, 352);
-            Addr_tb.Margin = new Padding(5, 5, 5, 5);
+            Addr_tb.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            Addr_tb.Location = new Point(199, 282);
+            Addr_tb.Margin = new Padding(4, 4, 4, 4);
             Addr_tb.Multiline = true;
             Addr_tb.Name = "Addr_tb";
             Addr_tb.ReadOnly = true;
-            Addr_tb.Size = new Size(346, 88);
+            Addr_tb.Size = new Size(277, 71);
             Addr_tb.TabIndex = 14;
             // 
             // label5
@@ -117,10 +120,10 @@
             label5.AutoSize = true;
             label5.Font = new Font("Stencil", 16.2F, FontStyle.Bold);
             label5.ForeColor = Color.FromArgb(41, 128, 185);
-            label5.Location = new Point(21, 368);
-            label5.Margin = new Padding(5, 0, 5, 0);
+            label5.Location = new Point(17, 294);
+            label5.Margin = new Padding(4, 0, 4, 0);
             label5.Name = "label5";
-            label5.Size = new Size(164, 39);
+            label5.Size = new Size(143, 33);
             label5.TabIndex = 13;
             label5.Text = "Address";
             // 
@@ -128,11 +131,12 @@
             // 
             Service_tb.BackColor = SystemColors.ButtonFace;
             Service_tb.BorderStyle = BorderStyle.FixedSingle;
-            Service_tb.Location = new Point(249, 289);
-            Service_tb.Margin = new Padding(5, 5, 5, 5);
+            Service_tb.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            Service_tb.Location = new Point(199, 231);
+            Service_tb.Margin = new Padding(4, 4, 4, 4);
             Service_tb.Name = "Service_tb";
             Service_tb.ReadOnly = true;
-            Service_tb.Size = new Size(346, 31);
+            Service_tb.Size = new Size(277, 34);
             Service_tb.TabIndex = 16;
             // 
             // label6
@@ -140,10 +144,10 @@
             label6.AutoSize = true;
             label6.Font = new Font("Stencil", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label6.ForeColor = Color.FromArgb(41, 128, 185);
-            label6.Location = new Point(21, 289);
-            label6.Margin = new Padding(5, 0, 5, 0);
+            label6.Location = new Point(17, 231);
+            label6.Margin = new Padding(4, 0, 4, 0);
             label6.Name = "label6";
-            label6.Size = new Size(155, 39);
+            label6.Size = new Size(134, 33);
             label6.TabIndex = 15;
             label6.Text = "Service";
             // 
@@ -151,12 +155,13 @@
             // 
             Description_tb.BackColor = SystemColors.ButtonFace;
             Description_tb.BorderStyle = BorderStyle.FixedSingle;
-            Description_tb.Location = new Point(249, 480);
-            Description_tb.Margin = new Padding(5, 5, 5, 5);
+            Description_tb.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            Description_tb.Location = new Point(199, 384);
+            Description_tb.Margin = new Padding(4, 4, 4, 4);
             Description_tb.Multiline = true;
             Description_tb.Name = "Description_tb";
             Description_tb.ReadOnly = true;
-            Description_tb.Size = new Size(346, 246);
+            Description_tb.Size = new Size(277, 197);
             Description_tb.TabIndex = 18;
             // 
             // label7
@@ -164,10 +169,10 @@
             label7.AutoSize = true;
             label7.Font = new Font("Stencil", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label7.ForeColor = Color.FromArgb(41, 128, 185);
-            label7.Location = new Point(15, 480);
-            label7.Margin = new Padding(5, 0, 5, 0);
+            label7.Location = new Point(12, 384);
+            label7.Margin = new Padding(4, 0, 4, 0);
             label7.Name = "label7";
-            label7.Size = new Size(206, 33);
+            label7.Size = new Size(170, 27);
             label7.TabIndex = 17;
             label7.Text = "Description";
             // 
@@ -175,11 +180,10 @@
             // 
             button1.BackColor = Color.FromArgb(41, 128, 185);
             button1.Font = new Font("Century Gothic", 12F, FontStyle.Bold);
-            button1.ForeColor = Color.Black;
-            button1.Location = new Point(446, 745);
-            button1.Margin = new Padding(4, 4, 4, 4);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(357, 596);
             button1.Name = "button1";
-            button1.Size = new Size(224, 84);
+            button1.Size = new Size(179, 67);
             button1.TabIndex = 19;
             button1.Text = "Arrived";
             button1.UseVisualStyleBackColor = false;
@@ -187,12 +191,12 @@
             // 
             // WorkerAcceptedRequest
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             BackgroundImage = Properties.Resources.Proj_pic;
             BackgroundImageLayout = ImageLayout.Zoom;
-            ClientSize = new Size(672, 834);
+            ClientSize = new Size(538, 667);
             Controls.Add(button1);
             Controls.Add(Description_tb);
             Controls.Add(label7);
@@ -205,7 +209,6 @@
             Controls.Add(Name_tb);
             Controls.Add(label1);
             Controls.Add(label2);
-            Margin = new Padding(4, 4, 4, 4);
             MaximizeBox = false;
             Name = "WorkerAcceptedRequest";
             StartPosition = FormStartPosition.CenterScreen;
